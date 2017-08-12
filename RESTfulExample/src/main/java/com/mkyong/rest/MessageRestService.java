a2f0f7a75d83ad82d56d7715130d5d2f8f9084f8
SyamDinesh <syamdinesh@gmail.com>
2017-08-13 02:23:53 +0530

    First Restful project

---
+++ RESTfulExample/src/main/java/com/mkyong/rest/MessageRestService.java
@@ -0,0 +1,19 @@
+package com.mkyong.rest;
+
+import javax.xml.ws.Response;
+
+//http://localhost:8080/RESTfulExample/rest/message/hello%20world
+@Path("/message")
+public class MessageRestService {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+
+		String result = "Restful example : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}
+
+}
