2e229cb86148766dee8c79bd153265f3660284b9
SyamDinesh <syamdinesh@gmail.com>
2017-12-18 18:58:34 +0530

    These are some missing files

--- Corejava/src/com/java/core/streams/StreamsPractice.java
+++ Corejava/src/com/java/core/streams/StreamsPractice.java
@@ -1,6 +1,10 @@
 package com.java.core.streams;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StreamsPractice {
@@ -11,5 +15,40 @@ public class StreamsPractice {
 		ArrayList<Integer> al1=(ArrayList<Integer>)al.stream().filter(i->i%2==0).collect(Collectors.toList());
 		
 		System.out.println(al+"\n"+al1);
+		List<staff> li=Arrays.asList(new staff("Syam", 24,new BigDecimal(8000)),new staff("Dinesh", 24, new BigDecimal(9000)),new staff("SD", 24, new BigDecimal(10000)));
+		List<String> al2=li.stream().map(i->i.getName()).collect(Collectors.toList());
+		
+		System.out.println(al2);
+	}
+}
+
+class staff{
+	private String name;
+	private int age;
+	private BigDecimal salary;
+
+	public staff(String name, int age, BigDecimal salary) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public BigDecimal getSalary() {
+		return salary;
+	}
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
 	}
 }
